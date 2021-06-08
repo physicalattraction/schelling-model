56ce0bd040b608023b692ef49a169592a3429046
Erwin Rossen <erwin.rossen@fashion.cloud>
2021-06-08 23:11:56 +0200

    Refactoring

--- src/schelling.py
+++ src/schelling.py
@@ -3,10 +3,15 @@ Schelling model of segragation, based on https://www.binpress.com/simulating-seg
 """
 
 import copy
-import itertools
+import os.path
 import random
+from collections import namedtuple
+from typing import Dict, List
 
 import matplotlib.pyplot as plt
+
+Point = namedtuple('Point', ['x', 'y'])
+Race = int
 
 
 class Schelling:
@@ -14,29 +19,36 @@ class Schelling:
                  nr_iterations: int, nr_races: int = 2):
         self.width = width
         self.height = height
-        self.races = nr_races
+        self.nr_races = nr_races
         self.empty_ratio = empty_ratio
-        # If there are less than this percentage of similar people, you are unhappy
+        # If there are less than this percentage of similar people, an agent is unhappy
         self.similarity_threshold = similarity_threshold
-        self.n_iterations = nr_iterations
-        self.empty_houses = []
-        self.agents = {}
+        self.nr_iterations = nr_iterations
+        self.empty_houses: List[Point] = []
+        # Dictionary from point to race, e.g. {(26, 16): 1, (47, 15): 2, ...}
+        self.agents: Dict[Point, Race] = {}
 
     def populate(self):
-        self.all_houses = list(itertools.product(range(self.width), range(self.height)))
-        random.shuffle(self.all_houses)
+        all_houses = [
+            (x, y)
+            for x in range(self.width)
+            for y in range(self.height)
+        ]
+        random.shuffle(all_houses)
 
-        self.n_empty = int(self.empty_ratio * len(self.all_houses))
-        self.empty_houses = self.all_houses[:self.n_empty]
+        nr_empty = int(self.empty_ratio * len(all_houses))
+        self.empty_houses = all_houses[:nr_empty]
 
-        self.remaining_houses = self.all_houses[self.n_empty:]
-        houses_by_race = [self.remaining_houses[i::self.races] for i in range(self.races)]
-        for i in range(self.races):
-            # create agents for each race
-            self.agents.update(dict(zip(houses_by_race[i], [i + 1] * len(houses_by_race[i]))).items())
+        remaining_houses = all_houses[nr_empty:]
+        houses_by_race = [remaining_houses[i::self.nr_races] for i in range(self.nr_races)]
+        self.agents = {
+            house: i + 1
+            for i in range(self.nr_races)
+            for house in houses_by_race[i]
+        }
 
-    def is_unsatisfied(self, x: int, y: int) -> bool:
-
+    def is_satisfied(self, agent: Point) -> bool:
+        x, y = agent
         race = self.agents[(x, y)]
         count_similar = 0
         count_different = 0
@@ -100,36 +112,28 @@ class Schelling:
 
         if (count_similar + count_different) == 0:
             # Somebody with no neighbors at all is satisfied
-            return False
+            return True
         else:
-            return float(count_similar) / (count_similar + count_different) < self.similarity_threshold
+            return float(count_similar) / (count_similar + count_different) >= self.similarity_threshold
 
     def update(self):
-        for i in range(self.n_iterations):
-            self.old_agents = copy.deepcopy(self.agents)
-            n_changes = 0
-            for agent in self.old_agents:
-                if self.is_unsatisfied(agent[0], agent[1]):
+        for i in range(1, self.nr_iterations + 1):
+            old_agents = copy.deepcopy(self.agents)
+            nr_changes = 0
+            for agent in old_agents:
+                if not self.is_satisfied(agent):
                     agent_race = self.agents[agent]
                     empty_house = random.choice(self.empty_houses)
                     self.agents[empty_house] = agent_race
                     del self.agents[agent]
                     self.empty_houses.remove(empty_house)
                     self.empty_houses.append(agent)
-                    n_changes += 1
-            print(i, n_changes)
-            if n_changes == 0:
+                    nr_changes += 1
+            print(f'Iteration {i}/{self.nr_iterations}: {nr_changes} changes')
+            if nr_changes == 0:
                 break
 
-    def move_to_empty(self, x, y):
-        race = self.agents[(x, y)]
-        empty_house = random.choice(self.empty_houses)
-        self.updated_agents[empty_house] = race
-        del self.updated_agents[(x, y)]
-        self.empty_houses.remove(empty_house)
-        self.empty_houses.append((x, y))
-
-    def plot(self, title, file_name):
+    def plot(self, title: str, file_name: str):
         fig, ax = plt.subplots()
         # If you want to run the simulation with more than 7 colors, you should set agent_colors accordingly
         agent_colors = {1: 'b', 2: 'r', 3: 'g', 4: 'c', 5: 'm', 6: 'y', 7: 'k'}
@@ -141,17 +145,19 @@ class Schelling:
         ax.set_ylim([0, self.height])
         ax.set_xticks([])
         ax.set_yticks([])
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        file_name = os.path.join(root_dir, 'tmp', file_name)
         plt.savefig(file_name)
 
 
 if __name__ == '__main__':
-    schelling_30 = Schelling(70, 70, 0.6, 0.3, 100, 7)
+    schelling_30 = Schelling(20, 20, 0.3, 0.3, 100, 2)
     schelling_30.populate()
     schelling_30.plot('Schelling Model with 7 colors: Initial State',
-                      'schelling_7_30_05_initial.png')
+                      'schelling_7_30_09_initial.png')
     schelling_30.update()
     schelling_30.plot('Schelling Model with 7 colors: Final State with Similarity Threshold 30%',
-                      'schelling_7_30_06_final.png')
+                      'schelling_7_30_10_final.png')
     #
     # schelling_50 = Schelling(50, 50, 0.3, 0.5, 100, 2)
     # schelling_50.populate()
